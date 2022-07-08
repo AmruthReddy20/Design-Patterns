298518b99cb0778e07b4cd5b1e1d3a55674f0a84
AmruthReddy20 <muthyalaamruthreddy@gmail.com>
2022-07-08 11:57:30 +0530

    Added Template, Visitor , composite

---
+++ template-method/java/App.java
@@ -0,0 +1,10 @@
+
+public class App {
+
+  public static void main(String[] args) {
+    var thief = new HalflingThief(new HitAndRunMethod());
+    thief.steal();
+    thief.changeMethod(new SubtleMethod());
+    thief.steal();
+  }
+}
